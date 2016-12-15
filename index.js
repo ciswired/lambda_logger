f1aed25028fed0aeb420e1a58288835d30ab532f
Sergey Andronnikov <sergey.andronnikov@altexsoft.com>
2016-12-15 13:11:32 +0200

    Utils section added

--- index.js
+++ index.js
@@ -794,4 +794,83 @@ var constantsObject = Object.freeze({
  */
 lambdaLogger.prototype.constants = constantsObject;
 
+
+/**
+ * Useful utils
+ * @type {{buildQueryString: utils.buildQueryString, objectsMerge: utils.objectsMerge}}
+ */
+var utils = {
+
+    /**
+     * Build query string for restHeart request from object
+     *
+     * @param inObject
+     * @returns {string}
+     * @private
+     */
+    buildQueryString: function (inObject) {
+        "use strict";
+        var queryString = '';
+        if (inObject.constructor === Object && Object.keys(inObject).length > 0) {
+            for (var index in inObject) {
+                if (inObject[index].constructor === Object) {
+                    if (Object.keys(inObject[index]).length > 0) {
+                        queryString += index + '=' + JSON.stringify(inObject[index]) + '&';
+                    }
+                } else if (inObject[index] instanceof Array && inObject[index].length > 0) {
+                    inObject[index].forEach(function (elem) {
+                        queryString += index + '=' + elem.toString() + '&';
+                    });
+                } else if (typeof inObject[index] === 'string' && inObject[index].length > 0) {
+                    queryString += index + '=' + inObject[index].toString().trim() + '&';
+                } else {
+                    queryString += index + '=' + inObject[index] + '&';
+                }
+            }
+        }
+        return queryString;
+    },
+
+    /**
+     * Merge several objects by keys
+     *
+     * @param target
+     * @returns {*}
+     * @see https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+     */
+    objectsMerge: function(target) {
+        if (target === undefined || target === null) {
+            throw new TypeError('Cannot convert first argument to object');
+        }
+
+        var to = Object(target);
+        for (var i = 1; i < arguments.length; i++) {
+            var nextSource = arguments[i];
+            if (nextSource === undefined || nextSource === null) {
+                continue;
+            }
+            nextSource = Object(nextSource);
+
+            var keysArray = Object.keys(nextSource);
+            for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+                var nextKey = keysArray[nextIndex];
+                var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+                if (desc !== undefined && desc.enumerable) {
+                    if (to[nextKey] !== undefined && to[nextKey] instanceof Array && nextSource[nextKey] instanceof Array) {
+                        for(var ikey in nextSource[nextKey]) {
+                            to[nextKey].push(nextSource[nextKey][ikey]);
+                        }
+                    } else {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+        }
+        return to;
+    }
+};
+
+lambdaLogger.prototype.utils = utils;
+
+
 module.exports = lambdaLogger;
